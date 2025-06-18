78b32d7777e389b09ac7d81bd52d09391ca5c439
Gustavo Acosta <gustavoacosta@example.com>
2025-06-18 17:54:00 -0600

    Creacion de la carpeta router y su acrivo

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
-const express  =require('express');
-const app = express();
 
-module.exports=app;
+const express = require('express');
+const app = express();
+const router = require('../routes/gymRouter'); // Asegúrate que la ruta sea correcta
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/gym', router); // Ruta corregida: 'gym' en lugar de 'gmy'
+
+module.exports = app;
