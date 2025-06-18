6ae9beb20a71f4ff25acc54c54c7b2db5a5458a1
Gustavo Acosta <gustavoacosta@example.com>
2025-06-18 17:00:40 -0600

    Primer commit de apires

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app/app')
+const config = require('./app/config/configuracion')
+
+app.listen(config.PORT,()=>{
+
+    console.log ("Servidor corriendo en el puerto", 
+    config.PORT);
+    
+})
