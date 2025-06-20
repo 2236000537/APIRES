3cc65764fc100829bfadbf7584f4f46755860569
Gustavo Acosta <gustavoacosta@example.com>
2025-06-20 17:04:01 -0600

    creamos el archivo conexion

---
+++ app/config/conexion.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose')
+const config =require ('./configuracion')
+
+module.exports={
+    connection  : null,
+    connect : ()=>{
+        if(this.connection) return this.connection
+        return mongoose.connect(config.DB)
+        .then(conn =>{
+            this.connect = conn
+            console.log ('SErealiso de manera correcta');
+
+        })
+        .catch(e =>{console.log('errpr en la conexion', e)})
+    }
+}
